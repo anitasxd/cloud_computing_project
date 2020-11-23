1c5ae43f64575527681bb272153f4bd7aafb58b8
Anita <anita.x.shen@disney.com>
2020-11-23 13:34:55 -0800

    change text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my Favorite Taiwanese Foods!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.allrecipes.com/recipe/17794/beef-noodle-soup/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Beef Noodle Soup
         </a>
       </header>
     </div>
